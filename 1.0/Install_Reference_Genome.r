087e04130ade51455ece21ae495eadc1ea00ea3c
Zhou Haobin <129016393+HaobinZhou@users.noreply.github.com>
2023-03-27 12:48:50 +0800

    Create Install_Reference_Genome.r

---
+++ 1.0/Install_Reference_Genome.r
@@ -0,0 +1,4 @@
+BiocManager::install("SNPlocs.Hsapiens.dbSNP155.GRCh37")
+BiocManager::install("BSgenome.Hsapiens.1000genomes.hs37d5")
+BiocManager::install("SNPlocs.Hsapiens.dbSNP155.GRCh38")
+BiocManager::install("BSgenome.Hsapiens.NCBI.GRCh38")
